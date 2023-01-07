4a89bcf6af619ef7e33764ebf481834990385bea
asahi-jp <miuraasahi1@gmail.com>
2023-01-07 21:56:13 +0900

    01/07

--- components/mainVisual.js
+++ components/mainVisual.js
@@ -1,9 +1,13 @@
 import { useEffect, useRef } from 'react';
 import { Box, Image, Text } from '@chakra-ui/react'
 import { useInView } from 'react-intersection-observer';
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import '@splidejs/splide/css'; 
 
 export default function MainVisual() {
-  const img = useRef()
+  const img1 = useRef()
+  const img2 = useRef()
+  const img3 = useRef()
   const texts = useRef()
   const { ref, inView } = useInView({
     rootMargin: '0px',
@@ -11,8 +15,9 @@ export default function MainVisual() {
 
   const onScroll = () => {
     const scrollY = window.pageYOffset
-    img.current.style.top = scrollY * 0.9 + "px"
-    // img.current.style.transform = `scale(${1 + scrollY * 0.0005})`
+    img1.current.style.top = scrollY * 0.9 + "px"
+    img2.current.style.top = scrollY * 0.9 + "px"
+    img3.current.style.top = scrollY * 0.9 + "px"
     texts.current.style.top = scrollY * 0.7 + "px"
   }
 
@@ -25,7 +30,7 @@ export default function MainVisual() {
 
   return (
     <>
-      <Box
+      {/* <Box
         ref={ref}
         position="relative"
         h="100vh"
@@ -48,6 +53,86 @@ export default function MainVisual() {
           height="100%"
           backgroundColor="black"
           opacity="0.7"
+        ></Box>
+        <Box
+          ref={texts}
+          position="absolute"
+          top="0"
+          width="100%"
+          height="100%"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text color="white" fontSize="5xl" transform="translateX(-50px)">Kyosuke</Text>
+          <Text color="white" fontSize="5xl" transform="translateX(50px)">Yamada</Text>
+        </Box>
+      </Box> */}
+
+      <Box
+        ref={ref}
+        position="relative"
+        h="100vh"
+        overflow="hidden"
+      >
+        <Splide
+          aria-label="私のお気に入りの画像集"
+          options={{
+            type: "loop",
+            speed: "5000",
+            padding: 0,
+            arrows: false,
+            pagination: false,
+            autoplay: true, // 自動再生を有効
+            interval: 5000, // 自動再生の間隔を3秒に設定
+            height: "100vh"
+          }}
+        >
+          <SplideSlide>
+            <Image
+              ref={img1}
+              src="/images/yamada_23.JPG"
+              alt='Dan Abramov' 
+              objectFit='cover'
+              position="absolute"
+              top="0"
+              width="100%"
+              height="100%"
+            />
+          </SplideSlide>
+          <SplideSlide>
+            <Image
+              ref={img2}
+              src="/images/yamada_01.jpg"
+              alt='Dan Abramov' 
+              objectFit='cover'
+              position="absolute"
+              top="0"
+              width="100%"
+              height="100%"
+            />
+          </SplideSlide>
+          <SplideSlide>
+            <Image
+              ref={img3}
+              src="/images/yamada_07.JPG"
+              alt='Dan Abramov' 
+              objectFit='cover'
+              position="absolute"
+              top="0"
+              width="100%"
+              height="100%"
+            />
+          </SplideSlide>
+        </Splide>
+        <Box
+          position="absolute"
+          top="0"
+          width="100%"
+          height="100%"
+          backgroundColor="black"
+          opacity="0.5"
         ></Box>
         <Box
           ref={texts}
